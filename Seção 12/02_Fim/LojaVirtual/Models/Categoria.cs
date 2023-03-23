7b04ba6b91846f5779f1c7ece962b9b1fdc4386c
wiliamfg <66524732+wiliamfg@users.noreply.github.com>
2023-03-23 20:43:10 -0300

    Commit Seção 12

---
+++ Seção 12/02_Fim/LojaVirtual/Models/Categoria.cs
@@ -0,0 +1,44 @@
+﻿using LojaVirtual.Libraries.Lang;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Models
+{
+    public class Categoria
+    {
+        [Display(Name = "Código")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
+        //TODO - Criar Validação - Nome Categoria único no banco de dados
+        public string Nome { get; set; }
+
+        /*
+         * Nome: Telefone sem fio
+         * Slug: telefone-sem-fio
+         * URL Normal: www.lojavirtual.com.br/Categoria/5
+         * URL Amigável: www.lojavirtual.com.br/Categoria/informatica
+         */
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E001")]
+        [MinLength(4, ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "MSG_E002")]
+        public string Slug { get; set; }
+
+        /*
+         * Auto-relacionamento
+         */
+        [Display(Name = "Categoria Pai")]
+        public int? CategoriaPaiId { get; set; }
+
+        /*
+         * ORM - EntityFrameworkCore
+         * 
+         */
+        [ForeignKey("CategoriaPaiId")]
+        public virtual Categoria CategoriaPai { get; set; }
+    }
+}
---
+++ Seção 12/02_Fim/LojaVirtual/Repositories/CategoriaRepository.cs
@@ -0,0 +1,59 @@
+﻿using LojaVirtual.Database;
+using LojaVirtual.Models;
+using LojaVirtual.Repositories.Contracts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+
+namespace LojaVirtual.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        const int _registroPorPagina = 10;
+
+        private LojaVirtualContext _banco;
+
+        public CategoriaRepository(LojaVirtualContext banco)
+        {
+            _banco = banco;
+        }
+
+        public void Atualizar(Categoria categoria)
+        {
+            _banco.Update(categoria);
+            _banco.SaveChanges();
+        }
+
+        public void Cadastrar(Categoria categoria)
+        {
+            _banco.Add(categoria);
+            _banco.SaveChanges();
+        }
+
+        public void Excluir(int Id)
+        {
+            Categoria categoria = ObterCategoria(Id);
+            _banco.Remove(categoria);
+            _banco.SaveChanges();
+        }
+
+        public Categoria ObterCategoria(int Id)
+        {
+            return _banco.Categorias.Find(Id);
+        }
+
+        public IPagedList<Categoria> ObterTodasCategorias(int? pagina)
+        {
+            int numeroPagina = pagina ?? 1;
+            return _banco.Categorias.Include(c => c.CategoriaPai).ToPagedList<Categoria>(numeroPagina, _registroPorPagina);
+        }
+
+        public IEnumerable<Categoria> ObterTodasCategorias()
+        {
+            return _banco.Categorias;
+        }
+    }
+}
